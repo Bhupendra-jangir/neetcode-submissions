a0a43b97b9dfe1e6271f7c8e42bcb4d1c330eca5
Bhupendra Jangir <bhupendrajangir97@gmail.com>
2026-09-08 13:47:46 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> a(26,0),b(26,0);
+        for(char ch:s){
+            a[ch-'a']++;
+        }
+        for(char ch:t) b[ch-'a']++;
+
+        return a==b;
+    }
+};
